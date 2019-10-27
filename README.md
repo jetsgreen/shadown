529b15203a806833fb23ede7edee2c8a5bb28dde
Felix Fernandez <fernandezfelix460.gmail.com>
2019-10-27 11:34:22 -0400

    created animated gifs, still need to add other functiions

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # shadown
+gif api key
+eXB5R1zPqIqwTl7XTFq8RkMNTH2Mrknc
+working api ex funny+cat
+https://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=eXB5R1zPqIqwTl7XTFq8RkMNTH2Mrknc
+
--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,54 @@
-$( document ).ready(function() {
+$(document).ready(function () {
+
+    var topics = ["new york jets", "dave chappelle", "tacos", "dominican republic"];
+
+
+    function renderButtons() {
+        $("#buttons-go-here").empty();
+
+        for (var i = 0; i < topics.length; i++) {
+
+            var gif = $("<button>");
+
+            gif.addClass("topics");
+
+            gif.attr("data-search", topics[i]);
+
+            gif.text(topics[i]);
+
+            $("#buttons-go-here").append(gif);
+        }
+
+    }
+    $("#add-button").submit(function (event) {
+        event.preventDefault();
+       
+        var textBox = $("#input").val().trim();
+        topics.push(textBox);
+        renderButtons();
+        console.log(topics);
+    });
+
+    renderButtons();
+
+    $(document).on("click", ".topics", function(){
+		var x = $(this).data("search");
+		console.log(x);
+
+		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=eXB5R1zPqIqwTl7XTFq8RkMNTH2Mrknc&limit=10"; 
+		console.log(queryURL);
+
+		$.ajax({url:queryURL, method: "GET"})
+			.done(function(response){
+				console.log(response);
+				for (var i=0; i < response.data.length; i++){
+					$("#gifs-go-here").prepend("<p>Rating: " + response.data[i].rating+"<p>");
+					$("#gifs-go-here").prepend("<img src='" + response.data[i].images.downsized.url + "'heigth=250px width=200px>");
+				}
+            })
+            
+				
+			});
     
-
-
-
     
 });
