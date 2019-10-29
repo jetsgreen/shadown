91711f237f5ba0cf22b9f1f539a3045df6cfd36b
Felix Fernandez <fernandezfelix460.gmail.com>
2019-10-29 12:35:19 -0400

    added flex box to the gif area

--- assets/main.js
+++ assets/main.js
@@ -1,26 +1,34 @@
 $(document).ready(function () {
+// create an array of topics
 
     var topics = ["new york jets", "dave chappelle", "tacos", "dominican republic"];
 
+// function that creates buttons
 
     function renderButtons() {
         $("#buttons-go-here").empty();
 
+// for loop to go through array
         for (var i = 0; i < topics.length; i++) {
 
-            var gif = $("<button>");
+// create a variable and give it a value of buttons, adding class of topics and passing attribute of data-SVGPathSegCurvetoQuadraticSmoothAbs,
+// and adding that button to the DOM
+            var btn = $("<button>");
 
-            gif.addClass("topics");
 
-            gif.attr("data-search", topics[i]);
+            btn.addClass("topics");
 
-            gif.text(topics[i]);
+            btn.attr("data-search", topics[i]);
 
-            $("#buttons-go-here").append(gif);
+            btn.text(topics[i]);
+
+            $("#buttons-go-here").append(btn);
         }
 
     }
+    // grabbing users input to create a button and dynamically adding it to the html
     $("#add-button").submit(function (event) {
+
         event.preventDefault();
        
         var userInput = $("#input").val().trim();
@@ -31,10 +39,12 @@ $(document).ready(function () {
 
     renderButtons();
 
+// creating a click event that will replace the topic every time users inputs value
     $(document).on("click", ".topics", function(){
 		var x = $(this).data("search");
-		console.log(x);
-
+        console.log(x);
+        
+// ajax call to the gif api and prepending ratings and img to html
 		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + x + "&api_key=eXB5R1zPqIqwTl7XTFq8RkMNTH2Mrknc&limit=10"; 
 		console.log(queryURL);
 
@@ -42,26 +52,15 @@ $(document).ready(function () {
 			.done(function(response){
 				console.log(response);
 				for (var i=0; i < response.data.length; i++){
-					$("#gifs-go-here").prepend("<p>Rating: " + response.data[i].rating+"<p>");
-					$("#gifs-go-here").prepend("<img src='" + response.data[i].images.downsized.url + "'heigth=400px, width=300px>");
+					$("#gif-area").prepend("<p>Rating: " + response.data[i].rating+"<p>");
+					$("#gif-area").prepend("<img src='" + response.data[i].images.downsized.url + "'heigth=200px, width=200px>");
 				}
             })
             
 				
             });
             
-            // function gifAnimate() {
-            //     var state = $(this).attr("data-state");
-            //     if (state === "still") {
-            //         $(this).attr("src", $(this).attr("data-animate"));
-            //         $(this).attr("data-state", "animate");
-            //     } else {
-            //         $(this).attr("src", $(this).attr("data-still"));
-            //         $(this).attr("data-state", "still");
+            
         
-            //     }
-            // }
         
-    
-    
 });
